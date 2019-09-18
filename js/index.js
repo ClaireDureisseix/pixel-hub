8967e7d8f59f98d5090d4fb8a6bbbe01a3810b62
Nicolas Le Pasteur <nicolas.lepasteur@gmail.com>
2019-09-18 15:47:35 +0200

    carousel automatic ok + dots

--- js/index.js
+++ js/index.js
@@ -16,17 +16,43 @@ showSlides();
 
 function showSlides(){
   const slides = document.querySelectorAll(".slides");
+  const dots = document.querySelectorAll(".dots");
   for (let i = 0; i < slides.length; i++){
     slides[i].style.display = "none";
   }
   slideIndex++;
   if (slideIndex > slides.length){
-    slideIndex = 1 /* pourquoi 1 plutôt que 0?*/
+    slideIndex = 1
   }
+  for (i = 0; i < dots.length; i++){
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
   slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
   setTimeout(showSlides, 3000);
 }
 // manual carousel
 
 
 
+
+
+/*automatic carousel ok
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides(){
+  const slides = document.querySelectorAll(".slides");
+  for (let i = 0; i < slides.length; i++){
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length){
+    slideIndex = 1
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 3000);
+}
+*/
