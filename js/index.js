ae5e53f7688735bf631ca2891b0466045411d2c0
Nicolas Le Pasteur <nicolas.lepasteur@gmail.com>
2019-09-18 11:17:58 +0200

    carousel 1

--- js/index.js
+++ js/index.js
@@ -6,3 +6,28 @@ function openContact() {
 function closeContact() {
   document.getElementById("contact").style.width = "0%";
 }
+
+
+
+
+// ci-dessous carousel
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides(){
+  let i = 0;
+  const slides = document.querySelectorAll(".slides");
+  for (i = 0; i < slides.length; i++){
+    slides[i].style.display = "none";
+  }
+  
+  slideIndex++;
+  if (slideIndex - 1 < slides.length){
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 1000)
+  }else{
+    slideIndex = 0
+    setTimeout(showSlides, 0);
+  }
+}
