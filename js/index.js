3d64005891f2a4bd98facdcfa000c6e9e20e1883
Artigan <a@ww4.eu>
2019-09-20 14:35:53 +0200

    menu pres

--- js/index.js
+++ js/index.js
@@ -1,10 +1,32 @@
 // CONTACT
 function openContact() {
-  document.getElementById("menu").style.height = "50%";
+  document.getElementById("menu").style.height = "40%";
 }
 
 function closeContact() {
   document.getElementById("menu").style.height = "0";
+}
+
+// Add active class to the current button (display none)
+var btnContainer = document.getElementById("contactBtn");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+// remove active class to the current button (display block)
+var btnContainer = document.getElementById("menu");
+var btns2 = btnContainer.getElementsByClassName("closebtn");
+for (var i = 0; i < btns2.length; i++) {
+  btns2[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += "";
+  });
 }
 
 
@@ -14,17 +36,17 @@ function closeContact() {
 let slideIndex = 0;
 showSlides();
 
-function showSlides(){
+function showSlides() {
   const slides = document.querySelectorAll(".slides");
   const dots = document.querySelectorAll(".dots");
-  for (let i = 0; i < slides.length; i++){
+  for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > slides.length){
+  if (slideIndex > slides.length) {
     slideIndex = 1
   }
-  for (i = 0; i < dots.length; i++){
+  for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
 
