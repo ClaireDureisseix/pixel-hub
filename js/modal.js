753530ab7fa515c1abaf7e73c8c243b9c0487f6c
Claire <clairepelletier.prod@gmail.com>
2019-10-02 14:13:40 +0200

    DONE: carousel works in modal

--- js/modal.js
+++ js/modal.js
@@ -1,34 +1,48 @@
 // MODAL GALLERY
 // Open the modal
-const modal = document.getElementById("myModal");
-const imgs = document.getElementsByClassName("portfolio-item");
-const modalImg = document.getElementById("images");
-for (img of imgs) {
-  img.addEventListener('click', e => {
-    const url = e.target.style.backgroundImage.split('"')[1];
-    modal.style.display = "block";
-    modalImg.src = url;
-    modalImg.style.width = "auto";
-  });
+const modal = document.getElementById("myModal")
+const imgs = document.getElementsByClassName("portfolio-item")
+const modalImg = document.getElementById("images")
+const next = document.getElementById('modal_button--next')
+const prev = document.getElementById('modal_button--prev')
+const portfolio1 = document.querySelector('.first')
+const portfolioChilds1 = portfolio1.children
+const portfolio2 = document.querySelector('.second')
+const portfolioChilds2 = portfolio2.children
+
+const modalCarousel = portfolioChilds => {
+  for(let i = 0; i < portfolioChilds.length; i++){
+    portfolioChilds[i].addEventListener('click', () =>{
+      let url = this.event.target.style.backgroundImage.split('"')[1];
+      modal.style.display = "block";
+      modalImg.src = url;
+      modalImg.style.width = "auto";
+      next.addEventListener('click', () =>{
+        if(i === portfolioChilds.length){
+          i = 0
+          url = portfolioChilds[i].style.backgroundImage.split('"')[1];
+        }else{
+          url = portfolioChilds[i++ + 1].style.backgroundImage.split('"')[1];
+        }
+        modalImg.src = url;
+      })
+      prev.addEventListener('click', () =>{
+        if(i === 0){
+          i = portfolioChilds.length
+          url = portfolioChilds[i].style.backgroundImage.split('"')[1];
+        }else{
+          url = portfolioChilds[i-- - 1].style.backgroundImage.split('"')[1];
+        }
+        modalImg.src = url;
+      })
+    })
+  }
 }
+modalCarousel(portfolioChilds1)
+modalCarousel(portfolioChilds2)
 
 // Closes the modal on click
 const closeBtn = document.getElementsByClassName("close")[0];
 closeBtn.addEventListener('click', () => {
   modal.style.display = "none";
 });
-
-// Carrousel in modal
-const next = document.getElementById('modal_button--next');
-const prev = document.getElementById('modal_button--prev');
-
-
-// let i = 0;
-// for (img of imgs) {
-//   let newUrl = imgs[i].style.backgroundImage.split('"')[1];
-//   i ++;
-//   console.log(newUrl);
-//   next.addEventListener('click', () => {
-//     modalImg.src = newUrl;
-//   });
-// }
