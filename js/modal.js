efa86e6767805e894112a379ea377b319507c3ee
Simon TSCHAN <tschan.omis@gmail.com>
2019-10-02 11:33:54 +0200

    modal-test

--- js/modal.js
+++ js/modal.js
@@ -3,14 +3,20 @@
 const modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-const img = document.getElementById("myImg");
+const imgs = document.getElementsByClassName("portfolio-item");
 const modalImg = document.getElementById("img01");
-const captionText = document.getElementById("caption");
-img.addEventListener('click', e => {
-  modal.style.display = "block";
-  modalImg.src = 'img/object/bus-lautaro-andreani.jpg';
-  modalImg.style.width = "auto";
-});
+console.log(imgs);
+
+for (img of imgs) {
+  img.addEventListener('click', (e) => {
+    /*const aff = e.target.style.backgroundImage.value;
+    modal.style.display = "block";
+    modalImg.src = 'img/object/bus-lautaro-andreani.jpg';
+    modalImg.style.width = "auto";*/
+    //e.target.style.backgroundImage =;
+    //modalImg.background = url
+  });
+}
 
 // Get the <span> element that closes the modal
 const span = document.getElementsByClassName("close")[0];
